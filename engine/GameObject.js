153371c1d57c3d0117e24f6cff0cda7d5ddaa697
bricks <bricks@unomaha.edu>
2025-09-22 11:52:16 -0500

    Start 2048

--- engine/GameObject.js
+++ engine/GameObject.js
@@ -9,7 +9,7 @@ class GameObject {
     hasStarted = false
     markForDelete = false
     name = "[NO NAME]"
-    constructor(name){
+    constructor(name) {
         this.addComponent(new Transform())
         this.name = name
     }
@@ -28,15 +28,18 @@ class GameObject {
             component.draw(ctx)
         }
     }
-    addComponent(component, values){
+    addComponent(component, values) {
         this.components.push(component)
         component.gameObject = this
         Object.assign(component, values)
     }
-    get transform(){
+    get transform() {
         return this.components[0]
     }
-    destroy(){
+    destroy() {
         this.markForDelete = true
+    }
+    getComponent(type) {
+        return this.components.find(go => go instanceof type)
     }
 }
